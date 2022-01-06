1e74e1dfa1597b554bebc239cfe227a0d7de0962
Shubham Chaudhary <shubham@chaudhary.xyz>
2022-01-05 22:39:05 -0500

    Read data from .env

--- coto/data.py
+++ coto/data.py
@@ -1,15 +1,19 @@
+import os
 from collections import namedtuple
 
 HealthInfo = namedtuple("HealthInfo", "hc_num, hc_suffix, hc_back, dob, postal_code, email, phone")
 
 
 def get_data() -> HealthInfo:
+    from dotenv import load_dotenv
+
+    load_dotenv()  # take environment variables from .env
     return HealthInfo(
-        '',
-        '',
-        '',
-        '',
-        '',
-        'shubham@chaudhary.xyz',
-        ''
+        os.getenv('hc_num'),
+        os.getenv('hc_suffix'),
+        os.getenv('hc_back'),
+        os.getenv('dob'),
+        os.getenv('postal_code'),
+        os.getenv('email'),
+        os.getenv('phone'),
     )

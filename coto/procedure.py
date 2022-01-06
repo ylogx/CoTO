3651276891d6a27c3d95006b14f316afcdfd1009
Shubham Chaudhary <shubham@chaudhary.xyz>
2022-01-05 19:58:29 -0500

    Initial selenium setup done

--- coto/procedure.py
+++ coto/procedure.py
@@ -1,7 +1,10 @@
+import time
+
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 def new_slot(driver: WebDriver):
-    driver.get("http://selenium.dev")
+    driver.get("https://shubham.chaudhary.xyz/blog/")
 
+    time.sleep(5)
     driver.quit()

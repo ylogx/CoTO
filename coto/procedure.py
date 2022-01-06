6c37ceb4b48767ba9c38473016bf347426988239
Shubham Chaudhary <shubham@chaudhary.xyz>
2022-01-05 20:15:36 -0500

    Add logger using loguru package

--- coto/procedure.py
+++ coto/procedure.py
@@ -1,10 +1,17 @@
 import time
 
+from loguru import logger
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def new_slot(driver: WebDriver):
     driver.get("https://shubham.chaudhary.xyz/blog/")
 
-    time.sleep(5)
+    logger.debug(f"Driver properties: {driver.__dict__}")
+    logger.info(f"Opened page titled: {driver.title}")
+
+    WebDriverWait(driver, timeout=10).until_not(lambda _: time.sleep(5))
+
     driver.quit()

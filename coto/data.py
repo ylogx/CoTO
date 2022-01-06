05400ec1655b4bd8d646cc6e191176295e4e553a
Shubham Chaudhary <shubham@chaudhary.xyz>
2022-01-05 22:41:15 -0500

    Print debugging information

--- coto/data.py
+++ coto/data.py
@@ -1,19 +1,25 @@
 import os
 from collections import namedtuple
 
-HealthInfo = namedtuple("HealthInfo", "hc_num, hc_suffix, hc_back, dob, postal_code, email, phone")
+from loguru import logger
+
+HealthInfo = namedtuple(
+    "HealthInfo", "hc_num, hc_suffix, hc_back, dob, postal_code, email, phone"
+)
 
 
 def get_data() -> HealthInfo:
     from dotenv import load_dotenv
 
     load_dotenv()  # take environment variables from .env
-    return HealthInfo(
-        os.getenv('hc_num'),
-        os.getenv('hc_suffix'),
-        os.getenv('hc_back'),
-        os.getenv('dob'),
-        os.getenv('postal_code'),
-        os.getenv('email'),
-        os.getenv('phone'),
+    info = HealthInfo(
+        os.getenv("hc_num"),
+        os.getenv("hc_suffix"),
+        os.getenv("hc_back"),
+        os.getenv("dob"),
+        os.getenv("postal_code"),
+        os.getenv("email"),
+        os.getenv("phone"),
     )
+    logger.debug(f'Current info: {info}')
+    return info
